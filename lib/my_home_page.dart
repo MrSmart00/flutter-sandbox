de2328541a1470326b05195ddd4493ecd370cf62
MrSmart00 <nexearth@gmail.com>
2023-05-28 23:38:09 +0900

    Fix: update widget tests

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -10,16 +10,41 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  Color _backgroundColor = Colors.white;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+      if (_counter > 10) {
+        _backgroundColor = Colors.redAccent;
+      } else if (_counter >= 0) {
+        _backgroundColor = Colors.white;
+      }
+    });
+  }
+
+  void _resetCounter() {
+    setState(() {
+      _counter = 0;
+      _backgroundColor = Colors.white;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+      if (_counter < 0) {
+        _backgroundColor = Colors.blueAccent;
+      } else if (_counter < 10) {
+        _backgroundColor = Colors.white;
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: _backgroundColor,
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
@@ -35,13 +60,46 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const SizedBox(height: 20.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: _incrementCounter,
+                  child: const Text(
+                    '+',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )
+                ),
+                const SizedBox(width: 20.0),
+                OutlinedButton(
+                    onPressed: _resetCounter,
+                    child: const Text(
+                      'Reset',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    )
+                ),
+                const SizedBox(width: 20.0),
+                OutlinedButton(
+                  onPressed: _decrementCounter,
+                  child: const Text(
+                    '-',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    ),
+                  )
+                )
+              ],
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
